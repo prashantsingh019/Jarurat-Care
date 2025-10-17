eb98b250cb15c775a980ec1639f2f68d51f05a3e
Prashant Singh <ps731278@gmail.com>
2025-10-17 22:20:51 +0530

    Implemented routing using React Router DOM

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,28 @@
-import { useEffect } from "react";
-import { useSelector,useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { provideData } from "./redux/slices/userData.slice";
 import { fetchData } from "./api/apiData";
-import Card from "./component/Card";
+import { Outlet } from "react-router-dom";
+import Navbar from "./component/Navbar";
 
 const App = () => {
   const dispatch = useDispatch();
-  const data = useSelector((state)=>state.patients);
+  const data = useSelector((state) => state.patients);
   
-  
-
-   
-  useEffect(()=>{
-    const takeData = async()=>{
-       const data = await fetchData();
-       dispatch(provideData(data));
-    } 
+  useEffect(() => {
+    const takeData = async () => {
+      const data = await fetchData();
+      dispatch(provideData(data));
+    };
     takeData();
-  },[])
-  
+  }, []);
  
-
   return (
     <div className="app w-[98%] m-auto p-2">
-      <div className="nav flex justify-between items-center">
-        <h1 className="text-3xl">Jarurat Care 🩺</h1>
-        <ul className="flex items-center gap-10 text-xl">
-          <li>Home</li>
-          <li>Patients</li>
-          <li>About</li>
-        </ul>
-      </div>
-      <div className="app-body mt-5 flex gap-4 justify-center flex-wrap">
-          {data.map((user)=>{
-            return <Card user={user} key={user.id}/>;
-          })}
-      </div>
+      <Navbar/>
+      <Outlet />
     </div>
-  )
-}
+  );
+};
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,35 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { Provider } from 'react-redux';
-import { store } from './redux/store.js';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.js";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Gallery from "./component/Gallery.jsx";
+import SelectedUser from "./component/SelectedUser.jsx";
 
-createRoot(document.getElementById('root')).render(
+
+const routes = createBrowserRouter([
+  {
+    path: "",
+    element: <App />,
+    children: [
+      {
+        path: "",
+        element: <Gallery />,
+      },
+      {
+        path: "user/:id",
+        element: <SelectedUser />,
+      }
+     
+    ],
+  },
+]);
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={routes} />
     </Provider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
