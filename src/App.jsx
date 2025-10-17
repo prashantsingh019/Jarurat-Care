b364f5ccdb8cd98bc38c7b7ff4aed3cbdae4b67e
Prashant Singh <ps731278@gmail.com>
2025-10-17 12:20:08 +0530

    Initialized a react-project using vite with tailwind.

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+const App = () => {
+  return (
+    <div className="app w-[98%] m-auto p-2">
+      <div className="nav flex justify-between items-center">
+        <h1 className="text-3xl">Jarurat Care 🩺</h1>
+        <ul className="flex items-center gap-10 text-xl">
+          <li>Home</li>
+          <li>Patients</li>
+          <li>About</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default App;
